4076fdae43003a92e21733cf28379119cbbd1a53
Olivier Schwander <olivier.schwander@lip6.fr>
2020-06-25 16:07:35 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Torchspdnet',
+      version='0.1',
+      description='PyTorch implementation of SPDNet for second order neural networks',
+      author='Olivier Schwander',
+      author_email='olivier.schwander@lip6.fr',
+      url='https://gitlab.lip6.fr/schwander/torchspdnet',
+      packages=['torchspdnet'],
+     )
+
